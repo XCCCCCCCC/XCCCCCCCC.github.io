42d504bb991b5dcb1679d3c99ea29f3b61c60903
chen.xia <xiachen@zamplus.com>
2025-06-10 11:13:02 +0800

    feat(*): 新增docs下的vue3子站；

---
+++ decoration/vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/decoration/', // 👈 这里设置为子路径
+  build: {
+    outDir: '../docs/decoration', // 👈 输出到 Jekyll 项目下的 docs/vue3
+    emptyOutDir: true,
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
